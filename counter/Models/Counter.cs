5220cddfb3b4c38e3209de5bef6e7dfc65f64e3a
jeffizmyname <80201005+jeffizmyname@users.noreply.github.com>
2024-10-06 21:10:36 +0200

    poprawiono generacje 2 koloru na podstawie koloru ustawionego rzez uzytkownika

--- counter/Models/Counter.cs
+++ counter/Models/Counter.cs
@@ -3,6 +3,7 @@ using Microsoft.Maui.Controls;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,36 +14,35 @@ namespace counter.Models
 {
     public class Counter : INotifyPropertyChanged
     {
-        public string id { get; set; }
-
-        private int value;
-
+    
         public event PropertyChangedEventHandler PropertyChanged;
+        private int _value;
 
+        public string id { get; set; }
         public string name { get; set; }
         public int defaultVal { get; set; } = 0;
-
         public int Value
         {
-            get => value;
+            get => _value;
             set
             {
-                if (this.value != value)
-                {
-                    this.value = value;
-                    OnPropertyChanged(nameof(Value));
-                }
+                _value = value;
+                OnPropertyChanged(nameof(Value));
             }
         }
         public Color coutnerColor { get; set; }
         public Color secondaryColor { get; set; }
+
+
+        private float colorFactorL = 0.85f;
+        private float colorFactorD = 2.3f;
 
         public Counter(string id, string name, Color color, int defaultVal)
         {
             this.id = id;
             this.name = name;
             this.coutnerColor = color;
-            this.secondaryColor = CreateSecondaryColor(color, 0.85f);
+            this.secondaryColor = CreateSecondaryColor(color, colorFactorL, colorFactorD);
             this.defaultVal = defaultVal;
             this.Value = defaultVal;
         }
@@ -51,6 +51,7 @@ namespace counter.Models
         {
             name = (string)counterDic["Name"];
             coutnerColor = Color.FromArgb((string)counterDic["Color"]);
+            secondaryColor = CreateSecondaryColor(coutnerColor, colorFactorL, colorFactorD);
             defaultVal = Convert.ToInt32(counterDic["BaseValue"]);
             Value = Convert.ToInt32(counterDic["Value"]);
         }
@@ -60,20 +61,36 @@ namespace counter.Models
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
 
-        Color CreateSecondaryColor(Color color, float factor)
+        Color CreateSecondaryColor(Color color, float factorLight, float factorDark)
         {
-            factor = Math.Clamp(factor, 0f, 2f);
+            double luminance = (0.299 * color.Red + 0.587 * color.Green + 0.114 * color.Blue);
 
-            float red = color.Red * factor;
-            float green = color.Green * factor;
-            float blue = color.Blue * factor;
+            float factor;
 
-            red = Math.Clamp(red, 0f, 1f);
-            green = Math.Clamp(green, 0f, 1f);
-            blue = Math.Clamp(blue, 0f, 1f);
+            if (luminance > 0.3)
+            {
+                factor = Math.Clamp(1 - ((float)luminance * factorLight * 1), 0f, 1f);
+                Trace.WriteLine("light " + name + " " + luminance);
+            }
+            else if(luminance < 0.01)
+            {
+                factor = Math.Clamp(1 + ((1 - (float)luminance) * factorDark * 8), 0f, 8f);
+                Trace.WriteLine("dark1 " + name + " " + luminance);
+            }
+            else
+            {
+                factor = Math.Clamp(1 + ((1 - (float)luminance) * factorDark * 5), 0f, 5f);
+                Trace.WriteLine("dark2 " + name + " " + luminance);
+            }
+
+            float red = Math.Clamp(color.Red * factor, 0f, 1f);
+            float green = Math.Clamp(color.Green * factor, 0f, 1f);
+            float blue = Math.Clamp(color.Blue * factor, 0f, 1f);
 
             return new Color(red, green, blue, color.Alpha);
+
         }
+
 
     }
 }
