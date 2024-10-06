ef0753159cfdd90387238ae38ceca9e58f946c3e
jeffizmyname <80201005+jeffizmyname@users.noreply.github.com>
2024-10-06 21:11:58 +0200

    popup do potwierdzania np usuwania countera

---
+++ counter/Popups/ConfirmationPopup.xaml.cs
@@ -0,0 +1,27 @@
+using CommunityToolkit.Maui.Views;
+
+namespace counter.Popups;
+
+public partial class ConfirmationPopup : Popup
+{
+
+    public event Action<bool> OnResult;
+    public ConfirmationPopup(string Question)
+	{
+		InitializeComponent();
+        QuestionLabel.Text = Question;
+    }
+
+    private void OnYesClicked(object sender, EventArgs e)
+    {
+        OnResult?.Invoke(true);
+        Close();
+    }
+
+    private void OnNoClicked(object sender, EventArgs e)
+    {
+        OnResult?.Invoke(false);
+        Close();
+    }
+
+}
