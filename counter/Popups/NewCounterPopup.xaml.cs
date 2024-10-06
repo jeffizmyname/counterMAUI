0260bf1550816a9d0110f65be50776041ce2c0be
jeffizmyname <80201005+jeffizmyname@users.noreply.github.com>
2024-10-06 21:12:37 +0200

    przeniesione do folderu Popups

---
+++ counter/Popups/NewCounterPopup.xaml.cs
@@ -0,0 +1,63 @@
+using CommunityToolkit.Maui.Views;
+using counter.ViewModels;
+using Google.Cloud.Firestore;
+using System.Diagnostics;
+
+namespace counter.Popups;
+
+public partial class NewCounterPopup : Popup
+{
+    Color selectedColor = Color.FromRgb(0, 0, 0);
+    private CounterViewModel _viewModel;
+    FirestoreDb db;
+    public NewCounterPopup(CounterViewModel viewModel, FirestoreDb databaseRef)
+    {
+        InitializeComponent();
+        _viewModel = viewModel;
+        db = databaseRef;
+    }
+
+    private void OnSubmitClicked(object sender, EventArgs e)
+    {
+        string counterName = CounterNameEntry.Text;
+        int baseValue;
+
+        if (string.IsNullOrWhiteSpace(counterName))
+        {
+            counterName = "defaultCounterName";
+        }
+
+        if (string.IsNullOrWhiteSpace(StartCountervalue.Text))
+        {
+            baseValue = 1;
+        }
+        else
+        {
+            baseValue = int.TryParse(StartCountervalue.Text, out int parsedValue) ? parsedValue : 1;
+        }
+
+        _viewModel.AddCounter(counterName, baseValue, selectedColor);
+        
+        Close();
+    }
+
+    private void OnSliderValueChanged(object sender, ValueChangedEventArgs e)
+    {
+        int red = (int)RedSlider.Value;
+        int green = (int)GreenSlider.Value;
+        int blue = (int)BlueSlider.Value;
+
+        selectedColor = Color.FromRgb(red, green, blue);
+
+        SelectedColorBox.Color = selectedColor;
+        SelectedColorLabel.Text = $"Selected Color: RGB({red}, {green}, {blue})";
+    }
+
+    private void OnNumericEntryTextChanged(object sender, TextChangedEventArgs e)
+    {
+        if (!string.IsNullOrEmpty(e.NewTextValue) && !int.TryParse(e.NewTextValue, out _))
+        {
+            ((Entry)sender).Text = e.OldTextValue;
+        }
+    }
+}
