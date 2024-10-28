505b4631f8e7cd11368144e54e2c21d43c0e036e
stanisław <stasiek1489@gmail.com>
2024-10-28 17:56:15 +0100

    porawa responsywności na androidzie poprzez zmiane grdiDefinitions

--- counter/Popups/NewCounterPopup.xaml.cs
+++ counter/Popups/NewCounterPopup.xaml.cs
@@ -8,13 +8,24 @@ namespace counter.Popups;
 public partial class NewCounterPopup : Popup
 {
     Color selectedColor = Color.FromRgb(0, 0, 0);
-    private CounterViewModel _viewModel;
+    private CounterViewModel _viewModel; 
     FirestoreDb db;
     public NewCounterPopup(CounterViewModel viewModel, FirestoreDb databaseRef)
     {
         InitializeComponent();
         _viewModel = viewModel;
         db = databaseRef;
+
+        if(DeviceInfo.Platform == DevicePlatform.Android)
+        {
+            GridColor.RowDefinitions.Add(new RowDefinition { Height = new GridLength(6, GridUnitType.Star)});
+            GridColor.RowDefinitions.Add(new RowDefinition { Height = new GridLength(2, GridUnitType.Star) });
+        } else
+        {
+            GridColor.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(5, GridUnitType.Star) });
+            GridColor.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(2, GridUnitType.Star) });
+
+        }
     }
 
     private void OnSubmitClicked(object sender, EventArgs e)
@@ -50,7 +61,7 @@ public partial class NewCounterPopup : Popup
         selectedColor = Color.FromRgb(red, green, blue);
 
         SelectedColorBox.Color = selectedColor;
-        SelectedColorLabel.Text = $"Selected Color: RGB({red}, {green}, {blue})";
+        //SelectedColorLabel.Text = $"Selected Color: RGB({red}, {green}, {blue})";
     }
 
     private void OnNumericEntryTextChanged(object sender, TextChangedEventArgs e)
